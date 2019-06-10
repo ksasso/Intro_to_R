00aa4557d3995a104ac0d1b9dbafa9099c6e1280
Katie Sasso <ksasso@MBP2018-007.local>
2019-06-10 09:58:10 -0400

    working through data viz

---
+++ notes.R
@@ -0,0 +1,53 @@
+
+# SEARCH STUDY GUIDE FOR - 
+    # ADD IN  - to see where my content could be supplemented for teaching
+    # USE  - go back and use in cardinal code 
+        # glimpse() ACROSS MULTIPLE DFs USING A MAP FUNCITON  - DONE 
+        # geom_tile or geom_count to do visuals of cateogircal variable - DONE 
+        # complete to add in 0s for missing combos of date-event - DONE 
+
+## MUST REVIEW
+
+    ### PURRR SESSIONT
+    https://jrnold.github.io/r4ds-exercise-solutions/data-visualisation.html#exercise-3.2.5
+    
+    ###TIDY EVAL SESSION
+    
+    ## ggplot 2 session (themes, legends, titles)
+        # theme 
+        # geom_count
+        # LEGENDS
+        # SECTION 3.3 Good aes() options for 3rd variables - DIFFERENT AESTHETICS 
+            # #5) STROKE AESTHETIC 
+    # stroke aesthetic works with geom_point shapes that have EITHER
+    # a border(color = stroke), or inside color (fill = size )
+    
+  
+    
+    ## VARIABLE TYPES
+
+
+### PLAN FOR LESSON CONTENT
+
+# make a unique version of the R4DS complete guide -- fill in with excreses 
+    # similar to those in R4DS_exercise_solutions but with unique data set 
+
+# do the TIDY GREATEST HITS 
+    # and then work through excercises mixing and matching functions 
+    # from purr etc 
+
+#EXAMPLES
+https://datacarpentry.org/R-genomics/04-dplyr.html
+
+
+### TOP FUNCTION TO START USING 
+
+?map # fxn over multiple columns 
+?geom_count # plotting categorical variables
+?geom_tile # plotting categorical variables
+?count add_count
+?gather
+?filter_at, filter_if
+?expand.grid
+
+
